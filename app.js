22d7227c575e4cfdfcaf83a27001b7ce315d7f13
SaraZuhair <90957447+SaraZuhair@users.noreply.github.com>
2022-09-09 02:23:24 +0300

    some more  info about vue usage

--- app.js
+++ app.js
@@ -21,12 +21,20 @@ const app = Vue.createApp({
       this.counter--;
     },
 
+    // the best way to output anything is to use computed property in vue which helps vue knows which dependency needs tp be changed
+    // because while running methods vue will check all the methods available if they have that property that is being changed...etc.
     Sname(event, sec) {
-      this.name = event.target.value +' '+ sec;
+      this.name = event.target.value;
     },
 
     confirmed() {
       this.confirmname = this.name;
+    },
+
+    reset() {
+      this.confirmname= '';
+      this.counter=0;
+      this.name='';
     }
   }
 });
